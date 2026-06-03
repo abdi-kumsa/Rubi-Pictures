e74602be283f47b2e39cb4b6d314b5cef7a5424f
Abdii Kumsaa <109530418+abdi-kumsa@users.noreply.github.com>
2026-06-03 17:58:53 +0300

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => ({
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
     react(),
-    tailwindcss(),
+    tailwindcss(),  
   ],
   resolve: {
     alias: {
